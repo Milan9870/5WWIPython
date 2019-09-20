94ce76bfd6ce975bf380a5b90a3fb516248b524b
Milan De Smijter <milan.desmijter@sgsintpaulus.eu>
2019-09-20 12:01:56 +0200

    wet-van-Coulomb

---
+++ 04_Variabelen/wisselgeld.py
@@ -0,0 +1,29 @@
+# invoer
+eurocent = int(input('Geef aantal eurocent: '))
+
+# berekening
+aantal_muntstukken = eurocent // 100
+eurocent = eurocent % 100
+
+aantal_muntstukken += eurocent // 100
+eurocent = eurocent % 50
+
+aantal_muntstukken += (eurocent // 20)
+eurocent = eurocent % 20
+
+aantal_muntstukken += (eurocent // 10)
+eurocent = eurocent % 10
+
+aantal_muntstukken += (eurocent // 5)
+eurocent = eurocent % 5
+
+aantal_muntstukken += (eurocent // 2)
+eurocent = eurocent % 2
+
+
+
+
+
+
+
+# uitvoer
