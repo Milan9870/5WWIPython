2b96466dafd83c44af50829908e2eddaee07ada5
Milan De Smijter <milan.desmijter@sgsintpaulus.eu>
2019-09-20 11:25:10 +0200

    test-variabelen

--- 03_Expressies/patroontje.py
+++ 03_Expressies/patroontje.py
@@ -1,1 +1,2 @@
 print(5 * str(float(12)))
+print(5 * str(24/2))
---
+++ 04_Variabelen/Test.py
@@ -0,0 +1,20 @@
+# wisselen van waarde
+# x, y = 2, 'hallo'
+
+# x, y = y, x
+
+# print(x, y)
+
+# programma die de discriminant berekent
+# invoer
+a = float(input('a: '))
+b = float(input('b: '))
+c = float(input('c; '))
+
+print(a, b, c)
+# berekening
+discriminant = b**2 - (4 * a * c)
+
+# uitvoer
+print(discriminant)
+# --------------------------------------------------------------------------------------------------------------------
