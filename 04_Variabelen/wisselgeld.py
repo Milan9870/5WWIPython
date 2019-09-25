4d6acfdf32bcce4a88e5532f9be4a1d18d9d4ec8
Milan De Smijter <milan.desmijter@sgsintpaulus.eu>
2019-09-25 16:35:49 +0200

    wisselgeld

--- 04_Variabelen/wisselgeld.py
+++ 04_Variabelen/wisselgeld.py
@@ -3,26 +3,25 @@ eurocent = int(input('Geef aantal eurocent: '))
 
 # berekening
 aantal_muntstukken = eurocent // 100
-eurocent = eurocent % 100
+eurocentt = eurocent % 100
 
-aantal_muntstukken += eurocent // 100
-eurocent = eurocent % 50
+aantal_muntstukken += eurocentt // 50
+eurocentt = eurocentt % 50
 
-aantal_muntstukken += (eurocent // 20)
-eurocent = eurocent % 20
+aantal_muntstukken += (eurocentt // 20)
+eurocentt = eurocentt % 20
 
-aantal_muntstukken += (eurocent // 10)
-eurocent = eurocent % 10
+aantal_muntstukken += (eurocentt // 10)
+eurocentt = eurocentt % 10
 
-aantal_muntstukken += (eurocent // 5)
-eurocent = eurocent % 5
+aantal_muntstukken += (eurocentt // 5)
+eurocentt = eurocentt % 5
 
-aantal_muntstukken += (eurocent // 2)
-eurocent = eurocent % 2
+aantal_muntstukken += (eurocentt // 2)
+eurocentt = eurocentt % 2
 
-aantal_muntstukken += (eurocent // 1)
-eurocent = eurocent % 1
-
+aantal_muntstukken += (eurocentt // 1)
+eurocentt = eurocentt % 1
 
 
 
@@ -30,4 +29,4 @@ eurocent = eurocent % 1
 
 
 # uitvoer
-print(aantal_muntstukken, eurocent)
+print( str(eurocent) + ' cent kan je omwisselen in ' + str(aantal_muntstukken) + ' muntstukken')
