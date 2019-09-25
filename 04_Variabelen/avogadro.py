0e2706ed99046db87ee9a2e1757f703db9fb9562
Milan De Smijter <milan.desmijter@sgsintpaulus.eu>
2019-09-25 15:50:17 +0200

    avogadro

--- 04_Variabelen/avogadro.py
+++ 04_Variabelen/avogadro.py
@@ -1,7 +1,7 @@
 # invoer
 Ms=32.06
 NA= 6.020 * 10 ** 23
-mol = float(input('Geef een aantal deeltjes, uitgedrukt in mol  : '))
+mol = float(input( 'Geef een aantal deeltjes, uitgedrukt in mol  : ' ) )
 
 # berekening
 massa = Ms * mol
@@ -9,5 +9,5 @@ aantal_deeltjes = mol * NA
 
 
 # uitvoer
-print(massa)
-print(aantal_deeltjes)
+print( massa )
+print( aantal_deeltjes )
