59b077efbf4c5901fcdb75f7f75ebc4fae4e14a8
Milan De Smijter <milan.desmijter@sgsintpaulus.eu>
2019-09-25 15:49:02 +0200

    wet-van-Coulomb

---
+++ 04_Variabelen/avogadro.py
@@ -0,0 +1,13 @@
+# invoer
+Ms=32.06
+NA= 6.020 * 10 ** 23
+mol = float(input('Geef een aantal deeltjes, uitgedrukt in mol  : '))
+
+# berekening
+massa = Ms * mol
+aantal_deeltjes = mol * NA
+
+
+# uitvoer
+print(massa)
+print(aantal_deeltjes)
