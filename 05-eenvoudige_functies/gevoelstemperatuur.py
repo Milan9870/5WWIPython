da64bf8fee1f26064ab4c05c2ca7118749ce1dc7
Milan De Smijter <milan.desmijter@sgsintpaulus.eu>
2019-10-11 11:26:27 +0200

    gevoelstemperatuur

--- 05-eenvoudige_functies/gevoelstemperatuur.py
+++ 05-eenvoudige_functies/gevoelstemperatuur.py
@@ -3,7 +3,7 @@ temperatuur = float(input('Wat is de luchttemperatuur in graden Celsius : '))
 windsnelheid = float(input('Wat is de windsnelheid in meter per seconden : '))
 
 # berekening
-gevoelstemperatuur = 13.12 + (0.6215 * temperatuur) + ((0.3965 * temperatuur) - 11.37) * windsnelheid**0.16
+gevoelstemperatuur = 13.12 + (0.6215 * temperatuur) + ((0.3965 * temperatuur) - 11.37) * windsnelheid ** 0.16
 
 # uitvoer
 print(gevoelstemperatuur)
