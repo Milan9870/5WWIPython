5d56256c05d0821c72680678b6fb753a278b88a2
Milan De Smijter <milan.desmijter@sgsintpaulus.eu>
2019-10-28 09:42:52 +0100

    blad stten schaar

---
+++ 06-Condities/blad steen schaar.py
@@ -0,0 +1,26 @@
+# invoer
+speler1 = str(input('Blad, steen of schaar: '))
+speler2 = str(input('Blad, steen of schaar: '))
+
+# berekening
+if speler1 == speler2:
+    winnaar = 'onbeslist'
+elif speler1 == 'blad':
+    if speler2 == 'steen':
+        winnaar = 'speler 1 wint'
+    else:
+        winnaar = 'speler 2 wint'
+elif speler1 == 'steen':
+    if speler2 == 'blad':
+        winnaar = 'speler 2 wint'
+    else:
+        winnaar = 'speler 1 wint'
+else:
+    if speler2 == 'blad':
+        winnaar = 'speler 1 wint'
+    else:
+        winnaar = 'speler 2 wint'
+
+
+# uitvoer
+print(winnaar)
