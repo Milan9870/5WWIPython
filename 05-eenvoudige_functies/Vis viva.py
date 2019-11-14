f4cd6692a01f028d074d98a2bd47646497101bb2
Milan De Smijter <milan.desmijter@sgsintpaulus.eu>
2019-11-14 19:17:12 +0100

    vis viva

--- 05-eenvoudige_functies/Vis viva.py
+++ 05-eenvoudige_functies/Vis viva.py
@@ -8,11 +8,13 @@ from math import pi, sqrt
 grote_as = (geocentrische_gravitatieconstante * afstand) / (2 * geocentrische_gravitatieconstante - afstand * (snelheid**2))
 periode = 2 * pi * sqrt(grote_as**3 / geocentrische_gravitatieconstante)
 minuten = periode // 60
-rest = periode % 60
 uren = minuten // 60
 minuten = minuten % 60
 dagen = uren // 24
 uren = uren % 24
+minuten = int(minuten)
+uren = int(uren)
+dagen = int(dagen)
 
 # uitvoer
 print('grote as: ' + str(grote_as) + ' meter')
